19be6a5b9ad2e8628517ec5fd44453c41e63e866
Praful Gabhane <45679855+prafulgabhane@users.noreply.github.com>
2023-01-13 16:22:21 +0530

    Adding file to remote UC 7 branch

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LinkedListAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+            Console.WriteLine();
+            // linkedList.InsertAtPerticularPosition(2, 30);
+            //linkedList.Display();
+            //linkedList.popEnd();
+            //linkedList.Display();
+            linkedList.MsTest(30);
+            linkedList.Display();
+            Console.ReadLine();
+        }
+    }
+}
