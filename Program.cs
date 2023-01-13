143fdad75b73806645303b36f8484e0278d283e7
Praful Gabhane <45679855+prafulgabhane@users.noreply.github.com>
2023-01-13 16:22:52 +0530

    Adding file to remote UC 8

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LinkedListAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+            Console.WriteLine();
+            linkedList.InsertAtPerticularPosition(2, 40);
+            linkedList.Display();
+            Console.WriteLine();
+            //linkedList.popEnd();
+            //linkedList.Display();
+            linkedList.MsTest(30);
+            linkedList.Display();
+            Console.ReadLine();
+        }
+    }
+}
