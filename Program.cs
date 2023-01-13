dcd441c781974d96a6cebed5726b979ed014e805
Praful Gabhane <45679855+prafulgabhane@users.noreply.github.com>
2023-01-13 16:21:07 +0530

    Adding file to remote

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LinkedListAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            //linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+
+            linkedList.InsertAtPerticularPosition(2, 30);
+            linkedList.Display();
+            linkedList.pop();
+            linkedList.Display();
+            Console.ReadLine();
+
+        }
+    }
+}
